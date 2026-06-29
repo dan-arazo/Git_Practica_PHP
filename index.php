a85da1896990fa8d30385663835c8c7f2764dbf2
Daniel Arazo <pedrin_8@yahoo.com>
2026-06-28 21:08:03 -0600

    cambio para rama desarrollo 3

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ echo "<h1>Hola Mundo desde Git</h1>";
 echo "<p>Mi primer proyecto usando Git y GitHub.</p>";
 echo "<hr>";
 echo "<h3>Versión 2 del sistema</h3>";
-echo "desde la rama desarrollo";
+echo "desde la rama desarrollo enviado desde windows";
 ?>
