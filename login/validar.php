04acc8b6d4ef99b04414daec2bcb27775baa87dd
Daniel Arazo <pedrin_8@yahoo.com>
2026-06-29 19:42:13 -0600

    se ha creado la estrucutra del modulo login

---
+++ login/validar.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<h1>Hola Mundo desde Git</h1>";
+echo "<p>Mi primer proyecto usando Git y GitHub.</p>";
+echo "<hr>";
+echo "<h3>Versión 2 del sistema</h3>";
+echo "desde la rama desarrollo enviado desde windows";
+echo "<h2>Modificado desde la Mac tbn</h2>";
+?>
