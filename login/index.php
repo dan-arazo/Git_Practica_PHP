4d49e9145269094c7b7ddf6e1bd5e52b00abbe0b
Daniel Arazo <pedrin_8@yahoo.com>
2026-06-29 19:52:44 -0600

    se agrega pantalla inicial de login

--- login/index.php
+++ login/index.php
@@ -1,9 +1,3 @@
 <?php
-
-echo "<h1>Hola Mundo desde Git</h1>";
-echo "<p>Mi primer proyecto usando Git y GitHub.</p>";
-echo "<hr>";
-echo "<h3>Versión 2 del sistema</h3>";
-echo "desde la rama desarrollo enviado desde windows";
-echo "<h2>Modificado desde la Mac tbn</h2>";
+echo "hola desde login";
 ?>
